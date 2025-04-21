22c300e4131ea4f8cd5876ebb5741cac80fd274e
Alfonso Molero Farman <alfonsomolerofarman@gmail.com>
2025-04-21 11:01:16 -0500

    git init

---
+++ src/main/java/collections/world_control_collections/WorldControlCollectionsApplication.java
@@ -0,0 +1,13 @@
+package collections.world_control_collections;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorldControlCollectionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorldControlCollectionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/collections/world_control_collections/WorldControlCollectionsApplicationTests.java
@@ -0,0 +1,13 @@
+package collections.world_control_collections;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorldControlCollectionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
